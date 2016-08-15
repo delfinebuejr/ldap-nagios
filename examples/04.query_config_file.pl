7be8ab6ca21dabd934a06cb917b9b74e18c8bd9c
delfin.ebue@gmail.com <delfin.ebue@gmail.com>
2016-08-15 15:35:23 +0800

    added contactgroup sample

--- examples/04.query_config_file.pl
+++ examples/04.query_config_file.pl
@@ -10,8 +10,8 @@ use NagiosConfigObjects;
 
 
 my $file = '/tmp/nnit-dfie/contacts.cfg';
-my $filter = 'contact';
-#my $filter = 'contactgroup';
+#my $filter = 'contact';
+my $filter = 'contactgroup';
 
 my $obj = NagiosConfigObjects->new({
                                     file => $file,
@@ -23,12 +23,13 @@ my $obj = NagiosConfigObjects->new({
 my $test = $obj->get_allobjects;    # this returns a collection of configobject.pm objects
                                     # use configobject.pm methods to access data for each object content
 
-#print Dumper($test);
+print Dumper($test);
 
 
 foreach my $item (@{$test}) {  # iterate each HASH_ref
 
-    my $attr = 'contact_name';
+    my $attr = 'contactgroup_name';
+#    my $attr = 'contact_name';
     my $value = $item->get_data($attr);
     
     print "$value \n";
