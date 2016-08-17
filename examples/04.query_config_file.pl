4b6ff38d1b8ff2a96511d28110f11e0572874917
delfin.ebue@gmail.com <delfin.ebue@gmail.com>
2016-08-17 15:30:17 +0800

    fixed bug

--- examples/04.query_config_file.pl
+++ examples/04.query_config_file.pl
@@ -9,9 +9,9 @@ use Data::Dumper;
 use NagiosConfigObjects;
 
 
-my $file = '/tmp/nnit-dfie/contacts.cfg';
-#my $filter = 'contact';
-my $filter = 'contactgroup';
+my $file = '/tmp/dfie/contacts.cfg';
+my $filter = 'contact';
+#my $filter = 'contactgroup';
 
 my $obj = NagiosConfigObjects->new({
                                     file => $file,
@@ -23,16 +23,23 @@ my $obj = NagiosConfigObjects->new({
 my $test = $obj->get_allobjects;    # this returns a collection of configobject.pm objects
                                     # use configobject.pm methods to access data for each object content
 
-print Dumper($test);
+#print Dumper($test);
 
+
+my @arr1 = [];
 
 foreach my $item (@{$test}) {  # iterate each HASH_ref
 
-    my $attr = 'contactgroup_name';
-#    my $attr = 'contact_name';
+#    my $attr = 'contactgroup_name';
+    my $attr = 'contact_name';
     my $value = $item->get_data($attr);
     
-    print "$value \n";
+#   print "$value \n";
+#  print Dumper($item->{attribute});      # reaching in is a bad idea
+
+   push  @arr1, $item->{attribute};
+
 }
 
+print Dumper(@arr1);
 
